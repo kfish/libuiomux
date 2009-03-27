acba6c12ff68c81bf0bc64456d552f1f6b7dc675
Conrad Parker <conrad@metadecks.org>
2009-03-27 10:23:09 +0900

    add LGPL licensing text throughout

--- src/libuiomux/uiomux_private.h
+++ src/libuiomux/uiomux_private.h
@@ -1,3 +1,22 @@
+/*
+ * UIOMux: a conflict manager for system resources, including UIO devices.
+ * Copyright (C) 2009 Renesas Technology Corp.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA  02110-1301 USA
+ */
+
 #ifndef __UIOMUX_PRIVATE_H__
 #define __UIOMUX_PRIVATE_H__
 
